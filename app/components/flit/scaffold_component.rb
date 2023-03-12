6a38055dfe588795ae9259a21100518da6a8a5c8
Aaron F Stanton <afstanton@gmail.com>
2023-03-12 18:38:16 -0400

    Add body to scaffold

--- app/components/flit/scaffold_component.rb
+++ app/components/flit/scaffold_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class Flit::ScaffoldComponent < Flit::BaseComponent
-  def initialize(app_bar: nil, **)
+  def initialize(app_bar:, body:)
     super
 
     @app_bar = app_bar
+    @body = body
   end
 end
