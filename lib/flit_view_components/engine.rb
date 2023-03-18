6dff300ea1c81cad731733ed5ed9d83c4513be14
Aaron F Stanton <afstanton@gmail.com>
2023-03-18 19:42:42 -0400

    Hoping this will work for theming

--- lib/flit_view_components/engine.rb
+++ lib/flit_view_components/engine.rb
@@ -1,6 +1,10 @@
 require "rails/engine"
 require "view_component"
 
+module Flit; end
+
+require_relative "../../app/components/flit/theme"
+
 module FlitViewComponents
   class Engine < ::Rails::Engine
     isolate_namespace FlitViewComponents
